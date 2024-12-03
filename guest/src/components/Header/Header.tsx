e74e2de8b7dc7074c5824d76a6fe21318ff46edd
PhuongHieuHo <fuhi.dev@gmail.com>
2024-12-03 09:52:38 +0700

    Header

--- guest/src/components/Header/Header.tsx
+++ guest/src/components/Header/Header.tsx
@@ -1,3 +1,36 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router";
+import { MENUS } from "./menus";
+
 export const Header: React.FC<{}> = (props) => {
- return <div>Header</div>;
+ return (
+  <div className="flex items-center w-full mb-6 py-4">
+   <div className="flex items-center gap-1 ">
+    <img
+     src="https://www.nbyy.cc/static/mudvod/common/128-128-logo.png"
+     alt="Logo"
+     className="h-9"
+    />
+   </div>
+   <div className="flex items-center gap-6 ml-10">
+    {MENUS.map((menu, idx) => (
+     <Link to={menu.url} className="text-white hover:text-primary" key={idx}>
+      {menu.name}
+     </Link>
+    ))}
+   </div>
+   <div className="rounded px-3 py-2 bg-[#424348] w-[250px] flex gap-4 ml-6">
+    <input
+     className="border-none bg-transparent flex-1 w-full text-sm text-white placeholder:text-neutral-300"
+     placeholder="斗破苍穹"
+    />
+    <div className="h-full w-[1px] bg-[#5a5b5f]"></div>
+    <div className="flex items-center gap-1 text-primary text-sm">
+     <FontAwesomeIcon icon={faSearch} />
+     <span>搜索</span>
+    </div>
+   </div>
+  </div>
+ );
 };
---
+++ guest/src/components/Header/menus.ts
@@ -0,0 +1,18 @@
+export const MENUS = [
+ {
+  name: "电影",
+  url: "",
+ },
+ {
+  name: "电视剧",
+  url: "",
+ },
+ {
+  name: "综艺",
+  url: "",
+ },
+ {
+  name: "动漫",
+  url: "",
+ },
+];
